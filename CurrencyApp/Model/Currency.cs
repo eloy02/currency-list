6f6a29d7ccca7451ac98830d272163f923a538a6
eloy02 <tis.str02@gmail.com>
2021-11-11 01:25:21 +0500

    added model

---
+++ CurrencyApp/Model/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CurrencyApp.Model
+{
+    /// <summary>
+    /// Валюта
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="EngName"></param>
+    /// <param name="Nominal"></param>
+    /// <param name="ParentCode"></param>
+    public sealed record Currency(string Id, string Name, string? EngName, int Nominal, string ParentCode, List<CurrencyRate> Rates);
+}
---
+++ CurrencyApp/Model/CurrencyRate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyApp.Model
+{
+    /// <summary>
+    /// Курс валюты
+    /// </summary>
+    /// <param name="Currency">Валюта</param>
+    /// <param name="Value"></param>
+    /// <param name="LastUpdateDate"></param>
+    public sealed record CurrencyRate(Guid Id, Currency Currency, decimal Value, DateTimeOffset LastUpdateDate, string CurrencyId);
+}
