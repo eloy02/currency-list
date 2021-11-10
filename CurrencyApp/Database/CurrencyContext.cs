bb3f7fd6362aac40305038a08ba29a44d54fc141
eloy02 <tis.str02@gmail.com>
2021-11-11 01:25:29 +0500

    added db context

---
+++ CurrencyApp/Database/CurrencyContext.cs
@@ -0,0 +1,53 @@
+﻿using CurrencyApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyApp.Database
+{
+    public class CurrencyContext : DbContext
+    {
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyRate> CurrencyRates { get; set; }
+
+        public CurrencyContext(DbContextOptions<CurrencyContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>(ent =>
+            {
+                ent.ToTable(nameof(Currencies));
+
+                ent.HasKey(e => e.Id);
+
+                ent.Property(e => e.Id)
+                    .HasMaxLength(10);
+
+                ent.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(225);
+
+                ent.Property(e => e.EngName)
+                    .HasMaxLength(225);
+
+                ent.Property(e => e.ParentCode)
+                    .HasMaxLength(10);
+
+                ent.HasMany(e => e.Rates).WithOne(r => r.Currency);
+            });
+
+            modelBuilder.Entity<CurrencyRate>(ent =>
+            {
+                ent.ToTable(nameof(CurrencyRates));
+
+                ent.HasKey(e => e.Id);
+
+                ent.Property(e => e.Value)
+                    .IsRequired();
+
+                ent.HasOne(r => r.Currency).WithMany(c => c.Rates).HasForeignKey(r => r.CurrencyId);
+            });
+        }
+    }
+}
