bafbe15f3cc709554c9d419ac38a6fab5c001dd0
eloy02 <tis.str02@gmail.com>
2021-11-11 13:41:28 +0500

    added mediatr commands for save

---
+++ CurrencyApp/Commands/AddCurrencyCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace CurrencyApp.Commands
+{
+    internal class AddCurrencyCommand : IRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? EngName { get; set; }
+        public int Nominal { get; set; }
+        public string ParentCode { get; set; }
+
+        public AddCurrencyCommand(string id, string name, string? engName, int nominal, string parentCode)
+        {
+            Id = id;
+            Name = name;
+            EngName = engName;
+            Nominal = nominal;
+            ParentCode = parentCode;
+        }
+    }
+}
---
+++ CurrencyApp/Commands/AddCurrencyRateCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MediatR;
+
+namespace CurrencyApp.Commands
+{
+    internal class AddCurrencyRateCommand : IRequest
+    {
+        public string CurrencyId { get; set; }
+        public decimal Nominal { get; set; }
+        public DateTimeOffset CurrencyDate { get; set; }
+
+        public AddCurrencyRateCommand(string currencyId, decimal nominal, DateTimeOffset currencyDate)
+        {
+            CurrencyId = currencyId;
+            Nominal = nominal;
+            CurrencyDate = currencyDate;
+        }
+    }
+}
